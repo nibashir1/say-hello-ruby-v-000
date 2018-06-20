5b96610e0ff7709c0126f3f23087a46e1a99c0a2
Nimer Bashir <nibashir1@gmail.com>
2018-06-20 22:02:35 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,6 @@
 # Build your say_hello method here
-def say_hello(language = "Ruby", name)
+def say_hello(language = "Ruby Programer", name)
   puts "Hello #{name}!"
 end
-
+say_hello
 say_hello("Nimer")
-say_hello()
